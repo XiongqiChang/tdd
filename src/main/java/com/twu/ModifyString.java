5d6297f5fd6b9c7a9b6918445aa914bb6ebc3d0a
XiongqiChang <2838159948@qq.com>
2020-09-14 17:06:49 +0800

    完成低于30%的字符串类型不进行修改

---
+++ src/main/java/com/twu/ModifyString.java
@@ -0,0 +1,13 @@
+package com.twu;
+
+/**
+ * @Author: xqc
+ * @Date: 2020/9/14 - 09 - 14 - 17:01
+ * @Description: com.twu
+ * @version: 1.0
+ */
+public class ModifyString {
+    public String addMommy(String noVowels) {
+        return noVowels;
+    }
+}
--- src/test/java/MommifierTest.java
+++ src/test/java/MommifierTest.java
@@ -1,2 +1,23 @@
+import com.twu.ModifyString;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MommifierTest {
+
+    @Test
+    void string_no_vowels(){
+        String noVowels = "green";
+        ModifyString modifyString = new ModifyString();
+        String expected = modifyString.addMommy(noVowels);
+        assertEquals(expected,"green");
+    }
+
+    @Test
+    void string_vowels_less_thirty_percent(){
+        String noVowels = "green";
+        ModifyString modifyString = new ModifyString();
+        String expected = modifyString.addMommy(noVowels);
+        assertEquals(expected,"green");
+    }
 }
